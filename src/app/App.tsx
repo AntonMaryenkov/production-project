a8ca6de46b3d3c149c5849a35b4b47b27e8b0699
Anton Maryenkov <antonmaryenkov@gmail.com>
2023-02-06 16:23:54 +0700

    добавил AppRouter, конфиг для роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,18 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import { Suspense } from 'react';
+import {Link} from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useTheme} from 'app/providers/ThemeProvider';
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const {theme, toggleTheme} = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to='/'>Главная</Link>
       <Link to='/about'>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/about' element={<AboutPage />} />
-          <Route path='/' element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter/>
     </div>
   )
 }
