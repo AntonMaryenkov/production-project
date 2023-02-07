96304f74e2c8c7935363a6b21a9c0a5d6505485b
Anton Maryenkov <antonmaryenkov@gmail.com>
2023-02-07 14:02:29 +0700

    добавил svg-loader и file-loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,21 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
 
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
@@ -32,5 +46,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   return [
     typescriptLoader,
     cssLoaders,
+    svgLoader,
+    fileLoader,
   ];
-};
+}
