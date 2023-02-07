3fb03f5c1d414e7acba9f6a47d230d2110706331
Anton Maryenkov <antonmaryenkov@gmail.com>
2023-02-07 15:25:11 +0700

    добавил Sidebar в widgets

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,16 +1,20 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from "react-router-dom";
-import {routeConfig} from "shared/config/routeConfig/routeConfig";
+import React, { Suspense } from 'react';
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        {Object.values(routeConfig).map(({element, path}) => (
+        {Object.values(routeConfig).map(({ element, path }) => (
           <Route
             key={path}
             path={path}
-            element={element}
+            element={(
+              <div className='page-wrapper'>
+                {element}
+              </div>
+            )}
           />
         ))}
       </Routes>
