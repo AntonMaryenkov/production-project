2fa93125d1245a17ced5e952abfea403101f5f57
Anton Maryenkov <antonmaryenkov@gmail.com>
2023-02-15 12:45:00 +0700

    add storybook, loki

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,39 +4,25 @@ module.exports = {
 		'es2021': true,
 		'node': true
 	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:i18next/recommended'
-	],
+	'extends':
+		[
+			'eslint:recommended',
+			'plugin:react/recommended',
+			'plugin:@typescript-eslint/recommended',
+			'plugin:i18next/recommended',
+			'plugin:storybook/recommended'
+		],
 	'parser': '@typescript-eslint/parser',
 	'parserOptions': {
 		'ecmaVersion': 'latest',
 		'sourceType': 'module'
 	},
-	'plugins': [
-		'react',
-		'@typescript-eslint',
-		'i18next'
-	],
+	'plugins': ['react', '@typescript-eslint', 'i18next'],
 	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
+		'indent': ['error', 'tab'],
+		'linebreak-style': ['error', 'windows'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always'],
 		'react/react-in-jsx-scope': 'off',
 		'react/button-has-type': [2, {
 			'button': true,
@@ -61,14 +47,15 @@ module.exports = {
 			markupOnly: true,
 			ignoreAttribute: ['to', 'data-testid']
 		}],
-		'max-len': ['error', { code: 100, ignoreComments: true }]
+		'max-len': ['error', {
+			code: 100,
+			ignoreComments: true
+		}]
 	},
-	overrides: [
-		{
-			files: ['**/src/**/*.test.{ts,tsx}'],
-			rules: {
-				'i18next/no-literal-string': 'off',
-			}
+	overrides: [{
+		files: ['**/src/**/*.test.{ts,tsx}'],
+		rules: {
+			'i18next/no-literal-string': 'off'
 		}
-	]
+	}]
 };
