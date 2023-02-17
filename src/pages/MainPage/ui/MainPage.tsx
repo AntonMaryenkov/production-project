18ab08a9292ebc15da4d58e4b6cd65c97d707201
Anton Maryenkov <antonmaryenkov@gmail.com>
2023-02-17 10:06:08 +0700

    upgrade sidebar, add button themes, add ComponentRender, fix tests

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,10 @@
 import { useTranslation } from 'react-i18next';
-import { BugButton } from 'app/providers/ErrorBoundary';
 
 export default function MainPage() {
 	const { t } = useTranslation('main');
 
 	return (
 		<div>
-			<BugButton />
 			{t('Главная страница')}
 		</div>
 	);
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from './Sidebar';
-import { renderWithTranslation } from 'shared/lib/renderWithTranslation/renderWithTranslation';
+import { ComponentRender } from 'shared/config/test/componentRender/ComponentRender';
 
 describe('Sidebar', () => {
 	test('Test render', () => {
-		renderWithTranslation(<Sidebar />);
+		ComponentRender(<Sidebar />);
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 	});
 
 	test('Test toggle', () => {
-		renderWithTranslation(<Sidebar />);
+		ComponentRender(<Sidebar />);
 		const toggleBtn = screen.getByTestId('sidebar-toggle');
 		fireEvent.click(toggleBtn);
 		expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
