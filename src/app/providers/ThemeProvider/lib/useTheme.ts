e6fbcb43cc30b4629702d65f52dee9d7f0605b46
Anton Maryenkov <antonmaryenkov@gmail.com>
2023-03-13 15:15:21 +0700

    add new Theme, add react refresh plugin

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -2,15 +2,30 @@ import { useContext } from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
 
 interface useThemeResult {
-  toggleTheme: () => void;
-  theme: Theme;
+	toggleTheme: () => void;
+	theme: Theme;
 }
 
 export function useTheme(): useThemeResult {
 	const { theme, setTheme } = useContext(ThemeContext);
 
 	const toggleTheme = () => {
-		const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+		let newTheme: Theme;
+
+		switch (theme) {
+		case Theme.DARK:
+			newTheme = Theme.LIGHT;
+			break;
+		case Theme.LIGHT:
+			newTheme = Theme.ORANGE;
+			break;
+		case Theme.ORANGE:
+			newTheme = Theme.DARK;
+			break;
+		default:
+			newTheme = Theme.LIGHT;
+		}
+
 		setTheme?.(newTheme);
 		document.body.className = newTheme;
 		localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
