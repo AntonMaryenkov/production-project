47b1fe39a2b6a9b05bf5634832efa2609d9a1115
Anton Maryenkov <antonmaryenkov@gmail.com>
2023-05-26 17:27:05 +0700

    add netlify config, fix api config, production

--- src/pages/ArticlesDetailsPage/ui/ArticlesDetailsPage/ArticlesDetailsPage.async.tsx
+++ src/pages/ArticlesDetailsPage/ui/ArticlesDetailsPage/ArticlesDetailsPage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-export const ArticlesDetailsPageAsync = lazy(() => new Promise(resolve => {
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore
-	setTimeout(() => resolve(import('./ArticlesDetailsPage')), 400);
-}));
+export const ArticlesDetailsPageAsync = lazy(() => import('./ArticlesDetailsPage'));
